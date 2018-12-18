346f00cbbf70040acb5586b14b0fbe7d42ef8562
Stagiaire <stagiaire@bloom-multimedia.fr>
2018-12-18 14:29:19 +0100

    mise à jour texte + style

--- resources/js/components/ShipsList.vue
+++ resources/js/components/ShipsList.vue
@@ -1,5 +1,5 @@
 <template>
-    <table class="table table-hover table-dark">
+    <table class="table table-hover table-dark table-striped">
         <thead>
             <tr>
                 <th scope="col">#</th>
